5d4706e931039368056d09fa81d6f6f382ac1fac
rnekdls <daingu275@gmail.com>
2024-05-22 16:37:09 +0900

    version 1 finish

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,42 @@
 // Mobile Menu Toggle Active
 const mobileMenu = document.querySelector(".mobile-icon");
 
+// clone mobile menu
+const menu = document.querySelector(".navi");
+const info = document.querySelector(".info");
+const newMenuBox = document.querySelector(".new-mobile-menu");
+const newMenu = menu.cloneNode(true);
+const newInfo = info.cloneNode(true);
+
+newMenuBox.insertAdjacentElement("beforeend", newMenu);
+newMenuBox.insertAdjacentElement("beforeend", newInfo);
+
 mobileMenu.addEventListener("click", function () {
   this.classList.toggle("active");
 
+  const menuHeight = document.querySelector(".new-mobile-menu").scrollHeight;
+
   if (this.classList.contains("active")) {
     this.classList.remove("not-active");
+    // show menu
+    newMenuBox.style.height = `${menuHeight}px`;
   } else {
     this.classList.add("not-active");
+    // hide menu
+    newMenuBox.style.height = 0;
   }
 });
+
+// fixed header style change when scrolling down
+const header = document.querySelector(".header");
+
+function stickyHeader() {
+  const scry = window.scrollY;
+  if (scry > 0) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", stickyHeader);
